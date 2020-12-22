777a1c4e76788c6fa568d9a27dbfc6aee187edec
JhovinB <jbac.tec@gmail.com>
2020-12-22 12:21:37 -0500

    first commit

---
+++ Infraestructure/Migrations/202012151725334_v2.cs
@@ -0,0 +1,24 @@
+﻿namespace Infraestructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Students", "studentLastName", c => c.String(nullable: false));
+            AddColumn("dbo.Students", "studentCode", c => c.String(nullable: false));
+            AddColumn("dbo.Students", "startDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Students", "EnrollmentDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Students", "EnrollmentDate");
+            DropColumn("dbo.Students", "startDate");
+            DropColumn("dbo.Students", "studentCode");
+            DropColumn("dbo.Students", "studentLastName");
+        }
+    }
+}
---
+++ MVCAJAX/Controllers/StudentController.cs
@@ -0,0 +1,71 @@
+﻿using Domain;
+using MVCAJAX.Models;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAJAX.Controllers
+{
+    public class StudentController: Controller
+    {
+        private StudentService service = new StudentService();
+
+        // GET: Student
+
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentID,
+                             Address = c.studentAddress,
+                             Name = c.studentName,
+                             LastName = c.studentLastName,
+                             Code = c.studentCode,
+                             EnrollmentDate = c.EnrollmentDate,
+                             startDate = c.startDate,
+                         }
+                         ).ToList();
+
+            return View(model);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getStudent(string id)
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult createStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+        [HttpPost]
+        public JsonResult updateStudent(Student std, int id)
+        {
+            service.Update(std, id);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+        [HttpPost]
+        public JsonResult deleteStudent(int id)
+        {
+            service.Delete(id);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+    }
+}
